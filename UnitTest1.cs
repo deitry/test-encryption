3e2e5172893562fa3095d0000bd117a667999a07
Dmitriy Vornychev <dm.s.vornychev@outlook.com>
2022-11-27 23:11:36 +0100

    Add alternative encryption method

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -42,4 +42,40 @@ public class Tests
 
         Assert.That(decrypted, Is.EqualTo(original));
     }
+
+    public string Encrypt2(string str)
+    {
+        var result = string.Empty;
+
+        for (var i = 0; i < str.Length / 2; i++)
+        {
+            result += str[i * 2 + 1];
+            result += str[i * 2];
+        }
+
+        if (str.Length % 2 == 1)
+            result += str.Last();
+
+        return result;
+    }
+
+    [Test]
+    [TestCase("move", "omev")]
+    [TestCase("attackattack", "taatkctaatkc")]
+    [TestCase("attack", "taatkc")]
+    [TestCase("attack!attack!!", "taatkca!ttca!k!")]
+    [TestCase("go!", "og!")]
+    [TestCase("go", "og")]
+    [TestCase("o", "o")]
+    [TestCase("", "")]
+    public void Test2(string original, string expected)
+    {
+        var encrypted = Encrypt2(original);
+
+        Assert.That(encrypted, Is.EqualTo(expected));
+
+        var decrypted = Encrypt2(encrypted);
+
+        Assert.That(decrypted, Is.EqualTo(original));
+    }
 }
